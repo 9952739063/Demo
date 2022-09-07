9eb94f67284c75b193f75b5001f100262fb5fae4
HP <HP@DESKTOP-F3BGO5Q>
2022-09-07 13:24:26 +0530

    new commit

---
+++ CitizenService/src/main/java/com/citizenservice/app/entity/Citizen.java
@@ -0,0 +1,27 @@
+package com.citizenservice.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Citizen {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column(name = "vaccinationCenterId")
+	private int vaccinationCenterId;
+
+}
---
+++ CitizenService/src/test/java/com/citizenservice/app/CitizenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizenservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
